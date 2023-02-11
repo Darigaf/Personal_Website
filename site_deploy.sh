2d515eefb54bbcd2d685e5a8f2b7be28d999bf81
Mateus <mateusbernardo@protonmail.com>
2023-02-11 04:39:35 -0300

    added base website files

---
+++ site_deploy.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cp /home/merico/salvar/Personal_Website/src/* /home/merico/Lets_Encrypt/config/www/
+cp /home/merico/salvar/Personal_Website/imagens/* /home/merico/Lets_Encrypt/config/www/imagens/
