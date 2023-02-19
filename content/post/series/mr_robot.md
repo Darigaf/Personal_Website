b971dc110de964ef469c9fc7088be00ba890ed09
Mateus <mateusbernardo@protonmail.com>
2023-02-18 22:03:26 -0300

    added new page

---
+++ content/post/series/mr_robot.md
@@ -0,0 +1,6 @@
+---
+title: "Mr_robot"
+date: 2023-02-18T22:02:54-03:00
+draft: true
+---
+
--- site_deploy.sh
+++ site_deploy.sh
@@ -4,12 +4,13 @@ git add ./*
 echo "Insert git commit message and press enter"
 read mensagem
 git commit -am "$mensagem"
-read -n1 -p "Do you want to push the website changes to main? [y/n]" fazer
-case $fazer in
-	y|y) git push;;
-	n|N) echo "not pushed";;
-esac
+#read -n1 -p "Do you want to push the website changes to main? [y/n]" fazer
+git push
+#case $fazer in
+	#y|y) git push;;
+	#n|N) echo "not pushed";;
+#esac
 rm -dr /home/merico/Lets_Encrypt/config/www/*
 hugo && cp -r public/* /home/merico/Lets_Encrypt/config/www/
-ssh-add /home/merico/.ssh/.chave_rsync
-rsync -rtvzP public/* rsync@mateusbernardo.xyz:/var/www/blog/
+ssh-add /home/merico/.ssh/.chave_server
+rsync -rtvzP public/* root@mateusbernardo.xyz:/var/www/blog/
