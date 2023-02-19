25a98eb1dc50fe5fe62c8a602fb5bf10ecc0b1e2
Mateus <mateusbernardo@protonmail.com>
2023-02-18 21:45:36 -0300

    aopa

--- hugo/mateus/site_deploy.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-rm -dr /home/merico/Lets_Encrypt/config/www/*
-hugo && cp -r public/* /home/merico/Lets_Encrypt/config/www/
--- site_deploy.sh
+++ site_deploy.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
-
-cp /home/merico/salvar/Personal_Website/src/* /home/merico/Lets_Encrypt/config/www/
-cp /home/merico/salvar/Personal_Website/imagens/* /home/merico/Lets_Encrypt/config/www/imagens/
+eval 'ssh agent -s' && ssh-add ~/.ssh/.chave_gitea
+git add ./*
+echo "Insert git commit message and press enter"
+read mensagem
+git commit -am "$mensagem"
+read -n1 -p "Do you want to push the website changes to main? [y/n]" fazer
+case $fazer in
+	y|y) git push;;
+	n|N) echo "not pushed";;
+esac
+rm -dr /home/merico/Lets_Encrypt/config/www/*
+hugo && cp -r public/* /home/merico/Lets_Encrypt/config/www/
+ssh-add ~/.chave_rsync
+rsync -rtvzP public/* rsync@mateusbernardo.xyz:/var/www/blog/
