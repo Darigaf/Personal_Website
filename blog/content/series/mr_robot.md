acffc28cf1f5af7c34e27b4bc9422f999843392b
Mateus <mateusbernardo@protonmail.com>
2023-02-20 02:57:57 -0300

    aaa

--- blog/content/series/mr_robot.md
+++ blog/content/series/mr_robot.md
@@ -4,3 +4,4 @@ date: 2023-02-18T22:02:54-03:00
 draft: true
 ---
 
+# mr robot é tipo muito foda
--- blog/site_deploy.sh
+++ blog/site_deploy.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
-eval `ssh-agent -s` && ssh-add /home/merico/.ssh/.chave_gitea
-git add ./*
-echo "Insert git commit message and press enter"
-read mensagem
-git commit -am "$mensagem"
-#read -n1 -p "Do you want to push the website changes to main? [y/n]" fazer
-git push
-#case $fazer in
-	#y|y) git push;;
-	#n|N) echo "not pushed";;
-#esac
+eval `ssh-agent -s` 
+#ssh-add /home/merico/.ssh/.chave_gitea
+#git add ./*
+#echo "Insert git commit message and press enter"
+#read mensagem
+#git commit -am "$mensagem"
+##read -n1 -p "Do you want to push the website changes to main? [y/n]" fazer
+#git push
+##case $fazer in
+	##y|y) git push;;
+	##n|N) echo "not pushed";;
+##esac
 rm -dr /home/merico/Lets_Encrypt/config/www/*
 hugo && cp -r public/* /home/merico/Lets_Encrypt/config/www/
 ssh-add /home/merico/.ssh/.chave_server
